60a3895f017894fd2981aaee3713d27ad0c27e47
Phil <phil.munzke@icloud.com>
2026-02-25 15:09:43 +0100

    fixed crash because pypi didn't upload the fonts

--- setup.py
+++ setup.py
@@ -2,12 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name="easypygamewidgets",
-    version="2.1.0",
+    version="2.1.1",
     packages=find_packages(),
     install_requires=[
         "pygame",
         "requests"
     ],
+    include_package_data=True,
+    package_data={
+        "easypygamewidgets": [
+            "assets/fonts/roboto mono/RobotoMono-Regular.ttf",
+            "assets/fonts/roboto mono/OFL.txt",
+            "assets/fonts/emoji/NotoEmoji-Regular.ttf",
+            "assets/fonts/emoji/OFL.txt"
+        ]
+    },
     author="PizzaPost",
     description="Create GUIs for pygame.",
     long_description=open("README.md", encoding="utf-8").read(),
